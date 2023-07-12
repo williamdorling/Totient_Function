e50ecc5c7435cbf75736c31c5b3803364b3a88b2
William Dorling <williamdorling@hotmail.com>
2023-07-12 12:48:27 +0100

    refactor phi function to use product rule

--- Problems_69-73_totient_function.js
+++ Problems_69-73_totient_function.js
@@ -23,16 +23,16 @@ const primeDivisors = (n) => {
         n /= 2;
     }
 
-    // loop through 3,5,7, ... , sqrt(n), do the same as above for each
-    for (i=3; i <= Math.sqrt(n); i = i+2){
+    // loop through 3,5,7,9, ... , sqrt(n), do the same as above for each
+    for (i=3; i <= n**0.5 ; i = i+2){
         while (n % i === 0){
             divisors.push(i);
             n /= i;
         }
     }
 
-    // if divisors is still empty, then n must be prime
-    if (divisors.length === 0){
+    // if n is now a prime number >2
+    if (n>2){
         divisors.push(n);
     }
 
@@ -40,8 +40,41 @@ const primeDivisors = (n) => {
 
 }
 
+// returns the unique prime divisors of an integer n
+const uniquePrimeDivisors = (n) => {
+
+    let divisors = [];
+
+    // if multiple of 2 divide out by 2 until odd
+    if (n % 2 === 0){
+        divisors.push(2);
+    }
+    while (n % 2 === 0){
+        n /= 2;
+    }
+
+    // loop through 3,5,7,9, ... , sqrt(n), do the same as above for each
+    for (i=3; i <= n**0.5 ; i = i+2){
+        if (n % i === 0){
+            divisors.push(i);
+        }
+        while (n % i === 0){
+            n /= i;
+        }
+    }
+
+    // if n is now a prime number >2
+    if (n>2){
+        divisors.push(n);
+    }
+
+    return divisors;
+
+}
+
+
 // returns the number of positive integers <= n which are coprime to n.
-const phi = (n) => {
+const phiNaive = (n) => {
     let totient = 0;
     for (i = 1; i <= n; i++){
         if (gcd(i, n) === 1){
@@ -50,6 +83,19 @@ const phi = (n) => {
     }
     return totient;
 }
+
+// does as above using product rule for computing phi(n) based on prime factors of n:
+// phi(n) = n*(1 - 1/p1)*(1 - 1/p2)*...*(1 - 1/pn) for p1, p2, ..., pn the unique prime factors of n. 
+const phi = (n) => {
+    let totient = n;
+    const primeFactors = uniquePrimeDivisors(n);
+    for (p of primeFactors){
+        // totient *= (1 - 1/p);
+        totient = Math.round(totient*(1 - 1/p));
+    }
+    return totient;
+}
+
 
 // returns the value of n <= m for which n/phi(n) is a maximum
 const Problem69 = (m) =>{
@@ -64,4 +110,7 @@ const Problem69 = (m) =>{
     return output;
 }
 
-console.log(primeDivisors(1000000));
+console.log(Problem69(1000000));
+
+
+
